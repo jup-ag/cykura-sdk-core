c86ebfdc3e650fe88d37a419cb9baa180734f015
Aadhinana <ajibaa@gmail.com>
2021-12-25 13:47:52 +0530

    feat: add dist

---
+++ dist/entities/wsol.d.ts
@@ -0,0 +1,7 @@
+import { Token } from './token';
+/**
+ * Known WSOL implementation addresses, used in our implementation of Solana#wrapped
+ */
+export declare const WSOL: {
+    [chainId: number]: Token;
+};
