41ed3b96130213579ebd9a819a541d5954a118c1
zkwarlock <shardul.aeer@somaiya.edu>
2022-03-03 20:10:56 +0530

    cleanup: Remove validateAndParseAddress
    Leftover EVM function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export { computePriceImpact } from './computePriceImpact'
 export { sortedInsert } from './sortedInsert'
 export { sqrt } from './sqrt'
-export { validateAndParseAddress } from './validateAndParseAddress'
